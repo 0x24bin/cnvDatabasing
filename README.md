bdb0c0617e7d63ca9f1b0d4fe5dca349a4949f6a
raymond301 <raymond.moore301@gmail.com>
2016-05-26 21:41:36 -0500

    added script to pull down meta data.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Accepts one file, two possible formats, resulting from PatternCNV.
 
 
-Commandline examples:
+    Commandline examples:
     python loadSingleCNVFile.py -f segmentData/s_SN1_JW_CNV_seg.txt -t SEG -p Moore_Raymond -s ThisBigStudy -c Germline
     python loadSingleCNVFile.py -f exonData/s_SN1_JW_CNV.txt -t EXON -p Moore_Raymond -s ThisBigStudy -c Somatic
 
@@ -11,7 +11,14 @@ Commandline examples:
 Accepts one tab-delimited file, first column is samplename, which correspondes to filename.
 Load every file within given directory two possible formats, resulting from PatternCNV.
 
-Commandline examples:
+    Commandline examples:
     python loadMultiCNVDir_metaFile.py -f segmentInput_meta.tsv -d segmentData/ -t SEG
     python loadMultiCNVDir_metaFile.py -f exonInput_meta.tsv -d exonData/ -t EXON
 
+
+###queryMETA_oneAttribute.py
+Connects to database, meta. Query all samples that match just one atttribute and value.
+Currently prints all meta data to STDOUT.
+
+    Commandline examples:
+    python queryMETA_oneAttribute.py -a Biospecimen_tissue_source -v Blood
--- queryMETA_oneAttribute.py
+++ queryMETA_oneAttribute.py
@@ -3,6 +3,7 @@ __author__ = 'm088378'
 import sys, os, csv
 import pymongo
 from pymongo import MongoClient
+from bson.code import Code
 from bson.objectid import ObjectId
 from bson.json_util import dumps
 from bson.json_util import loads
@@ -26,16 +27,24 @@ cnvSmp = db["meta"]
 
 curBool = cnvSmp.find({args.attribute : {'$exists':1}})
 if curBool.count() < 1:
-    print("No Attribute Exists: {}".format(args.attribute))
+    sys.stderr.write("No Attribute Exists: {}\n\n".format(args.attribute))
     quit()
 
 
-mr = db.command({"mapreduce" : "meta",
-    "map" : function(){ for (var key in this) { emit(key, null); }},
-    "reduce" : function(key, stuff) { return null; },
-    "out": "meta" + "_keys"}
-)
+map = Code("function(){ for (var key in this) { emit(key, null); }}")
+reduce = Code("function(key, stuff) { return null; }")
+allFields = cnvSmp.map_reduce(map, reduce, "meta_keys")
+fieldList = []
+for doc in allFields.find():
+    if doc["_id"] == "_id":
+        continue
+    fieldList.append(doc["_id"])
 
-fieldList = db[mr.result].distinct("_id")
+sfieldList = sorted(fieldList)
+print( "DatabaseID\t{}".format("\t".join(sfieldList)) )
 
-pprint( fieldList )
+qMetaRez = cnvSmp.find({args.attribute : args.metavalue})
+for doc in qMetaRez:
+    vals = [doc[e] for e in sfieldList]
+    print("{}\t{}".format( str(doc['_id']),"\t".join(vals) ) )
+    #pprint(vals)
